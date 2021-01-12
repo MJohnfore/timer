d804941401d5516323d19a4d59bca19d36ce8136
MJohnfore <77157846+MJohnfore@users.noreply.github.com>
2021-01-11 23:00:18 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+num = 9
+start = False
 def on_forever():
-    pass
+    global num
+    while start = True:
+        basic.show_number(num)
+        basic.pause(350)
+        basic.show_number(num - 1)
 basic.forever(on_forever)
+
+def on_button_pressed_a():
+        pass
+    input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let num = 9
 basic.forever(function on_forever() {
+    
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(num)
+        basic.pause(350)
+        basic.showNumber(num - 1)
+    }
     
 })
